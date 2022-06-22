3dee2f14358289617a89c96c8116e1437beed34b
Jesús García Crespo <jesus@sevein.com>
2022-06-22 13:01:39 +0200

    Add PackagePendingAlert component
    This is the yellow alert widget shown above the navigation elements of the
    package page when a decision needs to be made by the user. Some layout changes
    were needed to allow the page to use all the space outside the container-xxl
    div.

---
+++ dashboard/src/components/PackagePendingAlert.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { usePackageStore } from "../stores/package";
+
+const packageStore = usePackageStore();
+</script>
+
+<template>
+  <div class="row" v-if="packageStore.isPending">
+    <div class="col-12">
+      <div
+        class="alert alert-warning border-0 border-rounded-0 rounded-0 text-center mb-0"
+        role="alert"
+      >
+        This package is mid-workflow, and is currently awaiting user
+        decision(s).
+      </div>
+    </div>
+  </div>
+</template>
